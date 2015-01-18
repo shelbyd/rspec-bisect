d1e457c416e81638d3bd9f14ff5294b6de62d2ce
Shelby Doolittle <shelby@shelbyd.com>
2015-01-18 12:16:33 -0800

    add origin branch to push gem version

--- scripts/release_version.sh
+++ scripts/release_version.sh
@@ -8,7 +8,7 @@ sed -i "s/VERSION.*/VERSION = \"$1\"/g" ./lib/rspec/bisect/version.rb &&
 git ci -am "version $1" &&
 git tag $1 &&
 git push &&
-git push $1 &&
+git push origin $1 &&
 rm *.gem &&
 gem build rspec-bisect.gemspec
 gem push "rspec-bisect-$1.gem"
